e062a18cf33f0f9d40ce238b17d1be66ba9787eb
Audouin Bulanhagui <audouin0033@gmail.com>
2019-10-31 14:33:58 +0800

    Fix bugs and finish the application

--- My-Practical/routes/index.js
+++ My-Practical/routes/index.js
@@ -23,4 +23,42 @@ router.get('/testconnect', function(req, res, next) {
   }
 });
 
+router.get('/select', function(req, res, next) {
+  db.query('SELECT * FROM items', function (err, rs) {
+    res.render('select', { inventory: rs });
+  });
+});
+
+router.get('/insert', function(req, res, next) {
+  res.render('insert', { inventory: {} });
+});
+
+router.post('/insert', function (req, res, next) {
+  db.query('INSERT INTO items SET ?', req.body, function (err, rs) {
+    res.send('Insert Sucess');
+  })
+})
+
+router.get('/delete', function (req, res, next) {
+  db.query('DELETE FROM items WHERE id = ?', req.query.id, function(err, rs) {
+    res.redirect('/select');
+  })
+});
+
+router.get('/edit', function (req, res, next) {
+  db.query('SELECT * FROM items WHERE id = ?', req.query.id, function(err, rs) {
+    res.render('insert', { inventory: rs[0] });
+  })
+});
+
+router.post('/edit', function (req, res, next) {
+  var param = [
+    req.body,         //data update
+    req.query.id      //condition update
+  ]
+  db.query('UPDATE items SET ? WHERE id = ?', param, function(err, rs) {
+    res.redirect("/select");
+  })
+})
+
 module.exports = router;
