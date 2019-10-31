732926b4ceedd071c3d85777eca2fd326c6d5925
Audouin Bulanhagui <audouin0033@gmail.com>
2019-10-31 13:57:08 +0800

    Successfully connected to database using Mysql

--- My-Practical/routes/index.js
+++ My-Practical/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'inventory',
+  debug: false
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/testconnect', function(req, res, next) {
+  if (db != null) {
+    res.send('Connection Success');
+  } else {
+    res.send('connection failed');
+  }
 });
 
 module.exports = router;
